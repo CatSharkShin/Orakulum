e634b93ccfffd7ea681ede59c91d5bc7e47289f8
Kóder László <koder.laci67@gmail.com>
2020-10-06 17:38:08 +0200

    Matek műv. app. megoldás ellenőrző elkezdése

--- private/js/matekmuveletApp.js
+++ private/js/matekmuveletApp.js
@@ -12,13 +12,49 @@ static createInput(app) {
     let b = Math.floor(Math.random() * 10) + 1;
     let random = Math.floor(Math.random() * 3);
     let o = operators[random];
+
     let para = d.createElement("P");
     para.innerText = a+""+o+""+b;
     app.appendChild(para);
 
+    let result = 0;
+    if (random == 0)
+    {
+        result = a + b;
+    }
+    if (random == 1)
+    {
+        result = a - b;
+    }
+    if (random == 2)
+    {
+        result = a * b;
+    }
+
     var x = d.createElement("INPUT");
     x.setAttribute("type", "text");
     app.appendChild(x);
+
+    let input = 0;
+
+    function getValueX() {
+        input = x.value;
+    }
+
+    
+    var btn = d.createElement("BUTTON");
+    btn.innerHTML = "Oké!";
+    btn.addEventListener("click", getValueX());
+    app.appendChild(btn);
+
+    if (input == result)
+    {
+        ach++;
+    }
+    else
+    {
+        ach--;
+    }
 
     }
 
