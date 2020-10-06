4b4b840a000e39b59125e9d826e866c25c36d34b
Kóder László <koder.laci67@gmail.com>
2020-10-06 15:59:44 +0200

    Matek műv. app. Randomszám generátor

--- private/js/matekmuveletApp.js
+++ private/js/matekmuveletApp.js
@@ -1,16 +1,25 @@
 <script>
 
 var d = document;
+var maxp = 5;
+var ach = 0;
 
 class MatekmuveletApp{
 
 static createInput(app) {
+    let operators = ["+","-","*","/"];
+    let a = Math.floor(Math.random() * 10) + 1;
+    let b = Math.floor(Math.random() * 10) + 1;
+    let random = Math.floor(Math.random() * 4);
+    let o = operators[random];
+    let para = d.createElement("P");
+    para.innerText = a+""+o+""+b;
+    app.appendChild(para);
 
-    
     }
 
 static checkInput(app) {
-
+    return {max: maxp, score: ach};
     }
 
 }
