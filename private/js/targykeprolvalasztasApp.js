51d78fa5210a93ef2f40d561d8df5ea26962a575
Kóder László <koder.laci67@gmail.com>
2020-10-06 14:12:00 +0200

    Tömbök létrehozása tárgynevek eltárolására

--- private/js/targykeprolvalasztasApp.js
+++ private/js/targykeprolvalasztasApp.js
@@ -7,8 +7,9 @@ var elertpont = 0;
 class TargykeprolvalasztasApp{
 
 static createInput(app) {
+    var objectsonPicture = ["TV","Zászló","Lego","Szék","Pohár","Könyv","Függöny","Ajtó","Laptop","Egér"];
+    var objectsnotonPicture = ["Csuklósbusz","Kutya","Váza","Seprű","Kolbász","Távcső","Sajt","Faliújság"];
 
-    
     }
 
 static checkInput(app) {
