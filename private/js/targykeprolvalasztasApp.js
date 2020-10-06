010e9ff7b22a3c0fb8de996098dda9eab44d5e30
Sirák Norbert <siraknorbert99@gmail.com>
2020-10-06 20:22:39 +0200

    Tárgy kiv. képről app - Stringek betöltése tömbbe random sorrendben

--- private/js/targykeprolvalasztasApp.js
+++ private/js/targykeprolvalasztasApp.js
@@ -6,28 +6,56 @@ var pointsAch = 0;
 
 class TargykeprolvalasztasApp{
 
-static createInput(app) {
-    
-    // Tömb, melyben a képen szereplő tárgyak vannak:
-    var objOnPic = [
-    	"TV", "Zászló", "Lego",
-    	"Szék", "Pohár", "Könyv",
-    	"Függöny", "Ajtó", "Laptop", "Egér"
-    	];
-    
-    // Tömb, melyben a képen NEM szereplő tárgyak vannak:
-    var objNotOnPic = [
-    	"Csuklósbusz", "Kutya", "Váza", 
-    	"Seprű", "Kolbász", "Távcső", 
-    	"Sajt", "Faliújság"
-    	];
+	static createInput(app) {
+	    
+	    // Tömb, melyben a képen szereplő tárgyak vannak:
+	    var objOnPic = [
+	    	"TV", "Zászló", "Lego",
+	    	"Szék", "Pohár", "Könyv",
+	    	"Függöny", "Ajtó", "Laptop"
+	    	];
 
-    }
+	    stringShuffleInArray(objOnPic);
+	    
+	    // Tömb, melyben a képen NEM szereplő tárgyak vannak:
+	    var objNotOnPic = [
+	    	"Csuklósbusz", "Kutya", "Váza", 
+	    	"Seprű", "Kolbász", "Távcső", 
+	    	"Sajt", "Faliújság", "Porsche"
+	    	];
 
-static checkInput(app) {
-    return {max: maxPoint, score: pointsAch};
-    }
+	    stringShuffleInArray(objNotOnPic);
 
-}
+	     // Funkció stringek összekeverésére egy tömbben:
+	    function stringShuffleInArray(stringArray) {
+	    	for (var i = stringArray.length - 1; i > 0; i--) {
+				var j = Math.floor(Math.random() * (i + 1));
+				var temp = stringArray[i];
+				stringArray[i] = stringArray[j];
+				stringArray[j] = temp;
+			}
+	    }
+
+	    // Képen szereplő és nem szereplő tárgyak betöltése egy tömbbe:
+	    var notObjIndex = 0;
+
+	    for (var i=0; i<maxPoint*2; i++){
+			if (i < maxPoint) {
+				allObjArray[i] = objOnPic[i];
+			} else {
+				allObjArray[i] = objNotOnPic[notObjIndex];
+				notObjIndex++;
+			}
+		}
+
+		stringShuffleInArray(allObjArray);
+
+	}
+
+	static checkInput(app) {
+	    return {max: maxPoint, score: pointsAch};
+	    }
+
+	}
 
 </script>
