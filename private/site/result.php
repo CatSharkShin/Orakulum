4637b80314f165da69f6055ca05dbc94ad72f936
CsontosDominik <61829086+CsontosDominik@users.noreply.github.com>
2020-10-07 16:38:56 +0200

    Design: result.php

--- private/site/result.php
+++ private/site/result.php
@@ -1,4 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+    h1{
+    color: #0C3683 !important;
+    font-size: 36px;
+    }
+    h2{
+    color: #0C3683 !important;
+    font-size: 36px;
+    }
+</style>
+</head>
+<body>
 <h1>Your test results: </h1>
 <h2 id="result"></h2>
-<input type="text" id="user_name" placeholder="Enter your name">
+<input type="text" id="user_name" class="btn btn-info" placeholder="Enter your name">
 <div id="error"></div>
+</body>
+</html>
