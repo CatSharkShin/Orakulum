1ebde528769cc057418bb201f3a085a0707355bf
Kóder László <koder.laci67@gmail.com>
2020-10-07 16:17:55 +0200

    Matek műv. app. kommentálása

--- private/js/matekmuveletApp.js
+++ private/js/matekmuveletApp.js
@@ -5,26 +5,28 @@ var maxp = 3;
 var ach = 0;
 
 class MatekmuveletApp{
-
+    //Inicializáció
     static createInput(app) {
         let operators = ["+","-","*"];
         let a, b, o, random, input, result;
         let iterator = 0;
 
         ujMuvelet();
-
+        //
         function ujMuvelet() {
+            //Randomszám és operátor generátor
             a = Math.floor(Math.random() * 10) + 1;
             b = Math.floor(Math.random() * 10) + 1;
             random = Math.floor(Math.random() * 3);
             o = operators[random];
 
+            //Új paragrafus létrehozása a művelet kiíratásának
             let para = d.createElement("P");
             para.innerText = a+""+o+""+b;
             app.appendChild(para);
 
             result = 0;
-
+            //Operátor vizsgálata
             if (random == 0) {
                 result = a + b;
             } else if (random == 1) {
@@ -32,11 +34,11 @@ class MatekmuveletApp{
             } else if (random == 2) {
                 result = a * b;
             }
-
+            //Textbox elkészítése
             var x = d.createElement("INPUT");
             x.setAttribute("type", "text");
             app.appendChild(x);
-        
+            //Gomb elkészítése
             var btn = d.createElement("BUTTON");
             btn.innerHTML = "Oké!";
             btn.addEventListener("click", function(){
@@ -47,7 +49,7 @@ class MatekmuveletApp{
                 
                 input = x.value;
                 iterator++;
-
+                //Pontszámok számítása
                 if (input == result) {
                     ach++;
                 } else {
